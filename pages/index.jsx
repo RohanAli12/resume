67ea32cd12276440312c61f9ae17c22da281057c
RohanAli12 <alirohan500@gmail.com>
2023-03-11 04:09:10 +0500

    update

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Clint | Front-End Developer</title>
+        <meta
+          name="description"
+          content="I'm a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences."
+        />
+        <link rel="icon" href="/fav.png" />
+      </Head>
+    </>
+  );
+}
